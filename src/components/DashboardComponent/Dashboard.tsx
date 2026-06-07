4e3c74261f26b3ee5e23bba9d78354e803a7d4bc
Luis <luis.contreras.dev.02@gmail.com>
2026-06-07 01:24:18 -0400

    Solucinando problema con comillas

--- src/components/DashboardComponent/Dashboard.tsx
+++ src/components/DashboardComponent/Dashboard.tsx
@@ -88,7 +88,7 @@ export default function DashboardComponent() {
             const userData = { nombre, email, password };
 
             try {
-                const response = await fetch('${import.meta.env.VITE_API_URL}/api/auth/register', {
+                const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/register`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -120,7 +120,7 @@ export default function DashboardComponent() {
         const loginData = { email, password };
 
         try {
-            const response = await fetch('http://localhost:4000/api/auth/login', {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
